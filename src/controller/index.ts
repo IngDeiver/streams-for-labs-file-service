5024b49fa5c215ea6e899fc386a95b890ab15e3e
Deiver <ingendeiver@gmail.com>
2021-02-27 12:58:12 -0500

    fix: separate photo service to other service different

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,8 +1,6 @@
 import FileControler from './FileController';
-import PhotoControler from './PhotoController';
 
 export {
   // eslint-disable-next-line import/prefer-default-export
   FileControler,
-  PhotoControler
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 import FileRouter from './file.route';
-import PhotoRouter from './photo.route';
 
 const router = Router();
 const prefix: string = '/api';
 
 router.use(`${prefix}/file`, FileRouter);
-router.use(`${prefix}/photo`, PhotoRouter);
 
 export default router;
